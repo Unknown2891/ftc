df325afd2e5fa6758c5170351222eac6ba40bf2a
Luqman Siddiqui <luqman.siddiqui154@gmail.com>
2024-12-22 14:48:31 -0600

    Update fieldcentricTelyOp.java

--- fieldcentricTelyOp.java
+++ fieldcentricTelyOp.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.Servo;
 
